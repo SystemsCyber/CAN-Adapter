79c10331adaeb3be91106b5dc82e30db9af4a4f1
Dr-Daily <56745553+Dr-Daily@users.noreply.github.com>
2023-07-12 10:17:06 -0400

    Update ArduinoMITM.ino

--- ArduinoMITM/ArduinoMITM.ino
+++ ArduinoMITM/ArduinoMITM.ino
@@ -65,22 +65,24 @@ Show the CAN messages as they come in over Serial. This function "
 is not necessary and is supplied for debugging.
 */
 void print_CAN(const CAN_message_t &msg){
-  Serial.print("MB "); Serial.print(msg.mb);
-  Serial.print("  OVERRUN: "); Serial.print(msg.flags.overrun);
-  Serial.print("  LEN: "); Serial.print(msg.len);
-  Serial.print(" EXT: "); Serial.print(msg.flags.extended);
-  Serial.print(" TS: "); Serial.print(msg.timestamp);
-  Serial.print(" ID: "); Serial.print(msg.id, HEX);
-  Serial.print(" Buffer: ");
+
+ // Serial.print("MB "); Serial.print(msg.mb);
+ // Serial.print("  OVERRUN: "); Serial.print(msg.flags.overrun);
+ // Serial.print("  LEN: "); Serial.print(msg.len);
+ // Serial.print(" EXT: "); Serial.print(msg.flags.extended);
+ // Serial.print(" TS: "); Serial.print(msg.timestamp);
+ // Serial.print(" ID: "); 
+  Serial.print(msg.id, HEX);
+  Serial.print(" ");
   for ( uint8_t i = 0; i < msg.len; i++ ) {
-    Serial.print(msg.buf[i], HEX); Serial.print(" ");
+    Serial.printf("%02X ",msg.buf[i]);
   } Serial.println();
 }
 
 void can1Sniff(const CAN_message_t &msg) {
   YELLOW_LED_state = !YELLOW_LED_state;
   yellow_toggle_timer = 0;
-  //print_can(msg); 
+  //print_CAN(msg); 
 
   //Make a new message based on the old one.
   CAN_message_t new_msg = msg;
@@ -90,19 +92,20 @@ void can1Sniff(const CAN_message_t &msg) {
   unsigned long pgn = (msg.id & 0x00FFFF00) >> 8;
   
   // Change the message values for each byte as you wish
-  if (pgn == 65265){ //0xFEF1
-    new_msg.buf[0]=0x00;
-    new_msg.buf[1]=0x11;
-    new_msg.buf[2]=0x22;
-    new_msg.buf[3]=0x33;
-    new_msg.buf[4]=0x44;
-    new_msg.buf[5]=0x55;
-    new_msg.buf[6]=0x66;
-    new_msg.buf[7]=0x77;
+  //0CEFC180#5A64FF00FFFFFFFF
+  if (pgn == 0xEFC1){ //0xFEF1
+    new_msg.buf[0]=byte(.2*msg.buf[0]);
+    // new_msg.buf[1]=0x11;
+    // new_msg.buf[2]=0x22;
+    // new_msg.buf[3]=0x33;
+    // new_msg.buf[4]=0x44;
+    // new_msg.buf[5]=0x55;
+    // new_msg.buf[6]=0x66;
+    // new_msg.buf[7]=0x77;
+    new_msg.id=0x0CEFC780;
   }
-  else if (pgn == 65248){ // Look for another PGN
-    new_msg.buf[0]=0x00;
-    new_msg.buf[3]=0x33;
+  else if (pgn == 0xEFC7){ // Look for another PGN
+    new_msg.id=0x0CEFC180;
   }
   else {
     //Do nothing. the new message is the same as the old one.
@@ -120,6 +123,7 @@ This routine is sends messages the other direction.
 void can2Sniff(const CAN_message_t &msg) {
   BLUE_LED_state = !BLUE_LED_state;
   blue_toggle_timer = 0;
+  //print_CAN(msg);
 
   CAN_message_t new_msg = msg;
   //Add code to look for Ids and manipulate them here.
